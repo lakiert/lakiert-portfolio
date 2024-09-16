881d21fe7ae43b1071ad29669985a0ff6551bfc8
lakiert <aleksandra.lakiert@gmail.com>
2024-09-16 14:53:06 +0300

    added a project

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ export default function App() {
       <h2 id="small-title">My projects</h2>
 
       <div className='list-container'>
-          <ListElement projectName={"Example company website"} linkProject={"https://lakiert.github.io/company_website"} linkGithub={"https://github.com/lakiert/company_website"}></ListElement>
+          <ListElement projectName={"Recipe app"} linkProject={"https://lakiert-delicious.netlify.app"} linkGithub={"https://github.com/lakiert/recipe_app"}></ListElement>
           <ListElement projectName={"To-do list"} linkProject={"https://lakiert-todo-list.netlify.app"} linkGithub={"https://github.com/lakiert/react-to-do-list"}></ListElement>
-          
+          <ListElement projectName={"Example company website"} linkProject={"https://lakiert.github.io/company_website"} linkGithub={"https://github.com/lakiert/company_website"}></ListElement>
       </div>
     </div>
   )
